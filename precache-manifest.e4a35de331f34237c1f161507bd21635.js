c77e92576486420ad57690814ec89625e2669966
Amin Aarab <amin.aarab@chasacademy.se>
2019-04-05 13:06:44 +0200

    Updates

---
+++ precache-manifest.e4a35de331f34237c1f161507bd21635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d9c99a4f19116c6d931",
+    "url": "/weather-app/static/js/runtime~main.8d9c99a4.js"
+  },
+  {
+    "revision": "12b077c6f4a2d21d1da4",
+    "url": "/weather-app/static/js/main.12b077c6.chunk.js"
+  },
+  {
+    "revision": "e069af532d7fab8916b4",
+    "url": "/weather-app/static/js/2.e069af53.chunk.js"
+  },
+  {
+    "revision": "12b077c6f4a2d21d1da4",
+    "url": "/weather-app/static/css/main.f420ec87.chunk.css"
+  },
+  {
+    "revision": "e069af532d7fab8916b4",
+    "url": "/weather-app/static/css/2.6aec2a72.chunk.css"
+  },
+  {
+    "revision": "b13aa578509bca881843ec42f6865ee4",
+    "url": "/weather-app/index.html"
+  }
+];
